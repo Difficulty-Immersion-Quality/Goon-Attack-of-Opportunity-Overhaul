ffb4b885ab0d929f9024ebb72b335135a39a2117
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2026-07-19 22:38:48 +1000

    Vanilla edits - Sentinel, War Caster, Polearm Master.

--- Goon's Attack of Opportunity Overhaul/Mods/Goon's Attack of Opportunity Overhaul/ScriptExtender/Lua/Client/_CriticalMissAoO.lua
+++ Goon's Attack of Opportunity Overhaul/Mods/Goon's Attack of Opportunity Overhaul/ScriptExtender/Lua/Client/_CriticalMissAoO.lua
@@ -8,60 +8,68 @@ local patchTargets = {
             Description = "h499b92e3gd78cg4436gb1f9g8ee659fc4e93;1",
         }
     },
+    Interrupt_WarCaster = {
+        Fields = {
+            -- Cast &lt;LSTag Type="Spell" Tooltip="Target_ShockingGrasp"&gt;Shocking Grasp&lt;/LSTag&gt; enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
+            Description = "had3e80e7g81f6g4fedg9938g5dd0a0f4be2e;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(false,true,context.Source))) and not HasSpellCastBlocked(context.Observer)",
+        }
+    },
     Goon_Interrupt_AttackOfOpportunity_Mainhand = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(false,true,context.Source))) and Goon_HasWeaponTypeInSlot(false,false,context.Observer)",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(false,true,context.Source))) and Goon_HasWeaponTypeInSlot(false,false,context.Observer)",
         }
     },
     Goon_Interrupt_AttackOfOpportunity_Offhand = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(true,true,context.Source))) and Goon_HasWeaponTypeInSlot(false,true,context.Observer)",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(true,true,context.Source))) and Goon_HasWeaponTypeInSlot(false,true,context.Observer)",
         }
     },
     Goon_Interrupt_AttackOfOpportunity_RangedMainhand = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "((Goon_AttackOfOpportunityConditions() and Goon_InRangedAttackRange(false,true) and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InRangedAttackRange(false,true,context.Source))) and Goon_HasWeaponTypeInSlot(true,false,context.Observer)",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and Goon_InRangedAttackRange(false,true) and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InRangedAttackRange(false,true,context.Source))) and Goon_HasWeaponTypeInSlot(true,false,context.Observer)",
         }
     },
     Goon_Interrupt_AttackOfOpportunity_RangedOffhand = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "((Goon_AttackOfOpportunityConditions() and Goon_InRangedAttackRange(true,true) and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InRangedAttackRange(true,true,context.Source))) and Goon_HasWeaponTypeInSlot(true,true,context.Observer)",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and Goon_InRangedAttackRange(true,true) and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InRangedAttackRange(true,true,context.Source))) and Goon_HasWeaponTypeInSlot(true,true,context.Observer)",
         }
     },
     Goon_Interrupt_AttackOfOpportunity_Unarmed = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "(Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(nil,true,context.Source))",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "(Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(nil,true,context.Source))",
         }
     },
     Goon_Interrupt_AttackOfOpportunity_Shove = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(nil,true,context.Source))) and (not Self() and CanShoveWeight() and IsMovable() and not Grounded() and not Tagged('GASEOUS_FORM') and not HasStatus('SG_Unconscious') and not Tagged('CANT_SHOVE_THROW'))",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(nil,true,context.Source))) and (not Self() and CanShoveWeight() and IsMovable() and not Grounded() and not Tagged('GASEOUS_FORM') and not HasStatus('SG_Unconscious') and not Tagged('CANT_SHOVE_THROW'))",
         }
     },
     Goon_Interrupt_AttackOfOpportunity_Grapple = {
         Fields = {
-            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
-            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(nil,true,context.Source))) and (not Self() and CanGrappleTargetSize() and IsMovable() and not Grounded() and not Tagged('GASEOUS_FORM') and not HasStatus('SG_Unconscious') and not Tagged('CANT_SHOVE_THROW'))",
             -- Attack an enemy that moves out of your reach or rolls a &lt;LSTag Tooltip="CriticalMiss"&gt;Critical Miss&lt;/LSTag&gt; when attacking you.
             Description = "hc3131359g5a0ag4c0agbe13gb6b30826c824;1",
+            InterruptContext = { "OnLeaveAttackRange", "OnCastHit" },
+            Conditions = "((Goon_AttackOfOpportunityConditions() and not IsAttack()) or (Goon_AttackOfOpportunityConditions_CriticalMiss() and Goon_InMeleeAttackRange(nil,true,context.Source))) and (not Self() and CanGrappleTargetSize() and IsMovable() and not Grounded() and not Tagged('GASEOUS_FORM') and not HasStatus('SG_Unconscious') and not Tagged('CANT_SHOVE_THROW'))",
         }
     },
 }
